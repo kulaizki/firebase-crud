1c7a14a551c6c9c4a5d069cdd2affd34e233e8bc
kulaizki <kulaizke@gmail.com>
2023-07-29 17:59:01 +0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  const [user, setUsers] = useState([]);
 
-    </div>
+  useEffect(() => {
+
+    
+  }, [])
+  return (
+    <div className="App"></div>
   );
 }
 
