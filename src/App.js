42ea3e24aabd23e148f060b54ad74269efd8359e
kulaizki <kulaizke@gmail.com>
2023-07-30 14:09:52 +0800

    CRU

--- src/App.js
+++ src/App.js
@@ -1,15 +1,77 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { db } from "./firebase-config";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 
 function App() {
-  const [user, setUsers] = useState([]);
+  const [newName, setNewName] = useState(""); // string
+  const [newAge, setNewAge] = useState(0); // number
+
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
 
   useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
 
-    
-  }, [])
+    getUsers();
+  }, []);
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className="App__create">
+        <input
+          placeholder="Name..."
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            setNewAge(event.target.value);
+          }}
+        />
+      </div>
+
+      <button onClick={createUser}> Create User</button>
+      {users.map((user) => {
+        return (
+          <div className="App__users" key={user.id}>
+            {" "}
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <button
+              onClick={() => {
+                updateUser(user.id, user.age);
+              }}
+            >
+              {" "}
+              Increase Age
+            </button>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
